c724b618ba22bb45c575d12ae007935179e9b228
Meena-Ola <123057820+Meena-Ola@users.noreply.github.com>
2023-05-14 21:10:33 +0700

    Add files via upload

---
+++ UI.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+#include <conio.h>
+#include <windows.h>
+class display{
+    public:
+    void welcome();
+    void loading(string);
+};
+void display::loading(string g){
+    system("cls");
+    cout<<"Loading "<<g<<"..."<<endl;
+    Sleep(100);
+    fflush(stdin);
+}
+void display::welcome(){
+    system("cls");
+    cout<<"__          __  _   "<<endl;
+    Sleep(100);
+    cout<<"\\ \\        / / | |"<<endl;
+    Sleep(100);
+    cout<<" \\ \\  /\\  / /__| | ___ ___  _ __ ___   ___  "<<endl;
+    Sleep(100);
+    cout<<"  \\ \\/  \\/ / _ \\ |/ __/ _ \\| \'_ ` _ \\ / _ \\"<<endl;
+    Sleep(100);
+    cout<<"   \\  /\\  /  __/ | (_| (_) | | | | | |  __/"<<endl;
+    Sleep(100);
+    cout<<"    \\/  \\/ \\___|_|\\___\\___/|_| |_| |_|\\___|"<<endl;
+    Sleep(100);
+}
